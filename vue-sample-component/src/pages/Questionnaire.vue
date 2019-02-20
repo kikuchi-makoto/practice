b36a5bf8c7cc646fa5890bc3265bdddd8be2e270
macotok <monochromer@r8.ucom.ne.jp>
2019-02-20 11:19:43 +0900

    add dom questionnaire

--- vue-sample-component/src/pages/Questionnaire.vue
+++ vue-sample-component/src/pages/Questionnaire.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    アンケート設定
+    <div>
+      アンケート設定
+    </div>
+    <div>
+      <ul>
+        <li>
+          <div>
+            <h2>コース名</h2>
+            <div>
+              <button type="button">編集</button>
+              <button type="button">削除</button>
+            </div>
+          </div>
+          <ul>
+            <li>
+              <div>
+                <h3>景品名</h3>
+                <div>
+                  <button type="button">編集</button>
+                  <button type="button">削除</button>
+                </div>
+                <p>有効</p>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <button type="button">コース追加</button>
+    </div>
   </div>
 </template>
