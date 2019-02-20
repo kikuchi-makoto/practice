67ac0be05029aef892323b0714096b5c41022b78
macotok <monochromer@r8.ucom.ne.jp>
2019-02-20 13:45:53 +0900

    add style button component

--- vue-sample-component/src/components/button/Add.vue
+++ vue-sample-component/src/components/button/Add.vue
@@ -1,5 +1,20 @@
+<style lang="scss" scoped>
+  @import "../../assets/sass/styles.scss";
+  .button {
+    padding: 0 20px;
+    box-sizing: border-box;
+    button {
+      padding: 10px 0;
+      width: 100%;
+      @include buttonColor();
+    }
+  }
+</style>
+
 <template>
-  <button type="button">{{text}}</button>
+  <div class="button">
+    <button type="button">{{text}}</button>
+  </div>
 </template>
 
 <script>
--- vue-sample-component/src/components/button/Delete.vue
+++ vue-sample-component/src/components/button/Delete.vue
@@ -1,5 +1,12 @@
+<style lang="scss" scoped>
+  @import "../../assets/sass/styles.scss";
+  .button {
+    @include buttonColor($red-color-01);
+  }
+</style>
+
 <template>
-  <button type="button">{{text}}</button>
+  <button type="button" class="button">{{text}}</button>
 </template>
 
 <script>
