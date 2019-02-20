a125d8a6a33e65ddf66de4f25bc4b08fdd0d4aa0
macotok <monochromer@r8.ucom.ne.jp>
2019-02-20 11:02:13 +0900

    add questionnaire

--- vue-sample-component/src/App.vue
+++ vue-sample-component/src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <questionnaire></questionnaire>
   </div>
 </template>
 
 <script>
+import Questionnaire from './pages/Questionnaire';
+
 export default {
-  name: 'App',
+  name: 'app',
+  components: {
+    Questionnaire,
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-sample-component/src/pages/Questionnaire.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    アンケート設定
+  </div>
+</template>
