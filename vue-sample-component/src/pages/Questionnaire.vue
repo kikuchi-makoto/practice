ff9a0fdb6df4c3822e09fbe80a6091f1b41ecaed
macotok <monochromer@r8.ucom.ne.jp>
2019-02-20 12:01:09 +0900

    add style questionnaire

--- vue-sample-component/src/pages/Questionnaire.vue
+++ vue-sample-component/src/pages/Questionnaire.vue
@@ -1,28 +1,101 @@
 <style lang="scss" scoped>
-  .block {
+  .l-contents-01 {
+    margin: 20px;
+    border: 1px solid #ddd;
     display: flex;
+    .m-label-01 {
+      width: 30%;
+      background-color: #eee;
+      padding: 20px;
+      box-sizing: border-box;
+    }
+    .m-box-01 {
+      width: 70%;
+      padding: 20px;
+      box-sizing: border-box;
+    }
+    .m-list-01 {
+      &-item {
+        margin-bottom: 20px;
+        padding-bottom: 20px;
+        border: 1px solid #ddd;
+      }
+      &-box {
+        display: flex;
+        padding: 10px;
+        background-color: #eee;
+        justify-content: space-between;
+      }
+    }
+    .m-list-02 {
+      border: 1px solid #ddd;
+      border-bottom: none;
+      margin: 20px;
+      &-item {
+        padding: 10px;
+        border-bottom: 1px solid #ddd;
+      }
+      &-box {
+        display: flex;
+      }
+      &-title {
+        margin-right: auto;
+      }
+    }
   }
 </style>
 
 <template>
-  <div class="block">
-    <div>
+  <div class="l-contents-01">
+    <div class="m-label-01">
       アンケート設定
     </div>
-    <div>
-      <ul>
-        <li>
-          <div>
-            <h2>コース名</h2>
+    <div class="m-box-01">
+      <ul class="m-list-01">
+        <li class="m-list-01-item">
+          <div class="m-list-01-box">
+            <h2>コース1</h2>
             <div>
               <button type="button">編集</button>
               <button type="button">削除</button>
             </div>
           </div>
-          <ul>
-            <li>
-              <div>
-                <h3>景品名</h3>
+          <ul class="m-list-02">
+            <li class="m-list-02-item">
+              <div class="m-list-02-box">
+                <h3 class="m-list-02-title">景品名1</h3>
+                <div>
+                  <button type="button">編集</button>
+                  <button type="button">削除</button>
+                </div>
+                <p>有効</p>
+              </div>
+            </li>
+            <li class="m-list-02-item">
+              <div class="m-list-02-box">
+                <h3 class="m-list-02-title">景品名2</h3>
+                <div>
+                  <button type="button">編集</button>
+                  <button type="button">削除</button>
+                </div>
+                <p>無効</p>
+              </div>
+            </li>
+          </ul>
+          <button type="button">景品追加</button>
+        </li>
+        <li class="m-list-01-item">
+          <div class="m-list-01-box">
+            <h2>コース2</h2>
+            <div>
+              <button type="button">編集</button>
+              <button type="button">削除</button>
+            </div>
+          </div>
+          <ul class="m-list-02">
+            <li class="m-list-02-item">
+              <div class="m-list-02-box">
+                <h3 class="m-list-02-title">景品名1</h3>
                 <div>
                   <button type="button">編集</button>
                   <button type="button">削除</button>
@@ -31,6 +104,7 @@
               </div>
             </li>
           </ul>
+          <button type="button">景品追加</button>
         </li>
       </ul>
       <button type="button">コース追加</button>
