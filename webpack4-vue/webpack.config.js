821e84194651cc53f9503397027ebd42f8a57454
macotok <monochromer@r8.ucom.ne.jp>
2019-02-25 20:00:38 +0900

    add splitChunks and compile scss

--- webpack4-vue/webpack.config.js
+++ webpack4-vue/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const AutoPrefixer = require('autoprefixer');
 
 module.exports = [
   {
     entry: {
       app: [
         './src/js/app.js',
+        './src/sass/style.scss',
       ],
     },
     output: {
@@ -32,10 +35,22 @@ module.exports = [
           test: /\.scss$/,
           use: [
             {
-              loader: 'style-loader',
+              loader: MiniCssExtractPlugin.loader,
             },
             {
               loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  AutoPrefixer(
+                    {
+                      browsers: ['last 2 versions', 'Android >= 4'],
+                    },
+                  ),
+                ],
+              },
             },
             {
               loader: 'sass-loader',
@@ -46,6 +61,16 @@ module.exports = [
     },
     resolve: {
       extensions: ['.js', '.vue'],
-    }
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/style.css',
+      }),
+    ],
   },
 ];
