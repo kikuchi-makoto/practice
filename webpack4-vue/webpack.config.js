9ce7fb2b42b652fc0a7ccab2d94e700bd13aa2e8
macotok <monochromer@r8.ucom.ne.jp>
2019-02-25 17:46:21 +0900

    add babel-loader

---
+++ webpack4-vue/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry: {
+      app: [
+        './src/js/app.js',
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].js',
+      publicPath: '/',
+    },
+    devServer: {
+      contentBase: './dist',
+      watchContentBase: true,
+      port: 3000,
+      open: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.vue'],
+    }
+  },
+];
